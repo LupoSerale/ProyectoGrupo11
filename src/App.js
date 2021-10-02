488df695dadd166f0764c47ecc46a8455f4cdc6b
ArturoCorpasUlloa <90206623+ArturoCorpasUlloa@users.noreply.github.com>
2021-10-02 14:24:39 -0500

    Registro de ventas

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import ListarProducto from './components/ListarProducto';
 import NuevoProducto from './components/NuevoProducto';
 import EditarProducto from './components/EditarProducto';
 import Home from './components/Home';
+import RegistrarVenta from './components/RegistrarVenta';
+import EstadoVenta from './components/EstadoVenta';
 function App() {
 
 
@@ -38,8 +40,15 @@ function App() {
             <li className="nav-item">
               <a className="nav-link" aria-current="page" href="/">Inicio</a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Ventas</a>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Ventas
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a className="dropdown-item" href="registrarVenta">Registrar Venta</a></li>
+                <li><a className="dropdown-item" href="estadoVenta">Estado Venta</a></li>
+              </ul>
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
@@ -74,6 +83,12 @@ function App() {
           </Route>
           <Route path="/editarProducto">
             <EditarProducto />
+          </Route>
+          <Route path="/registrarVenta">
+            <RegistrarVenta />
+          </Route>
+          <Route path="/estadoVenta">
+            <EstadoVenta />
           </Route>
           <Route path="/">
             <Home />
